53aa0dffdfc15be4dcb11f383d2e242fd3dcff90
Mehrdad Eshraghi <67071121+MehrdadEshraghi@users.noreply.github.com>
2022-02-21 19:56:39 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/hello', (req, res) => {
-  res.send('just hello.kk.');
+  res.send('just hello.');
 });
 
 app.get('/', (req, res) => {

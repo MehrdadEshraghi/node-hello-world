d8002c1c2265a76f2284004dfdc19e86708c5c29
Mehrdad Eshraghi <67071121+MehrdadEshraghi@users.noreply.github.com>
2022-02-22 11:24:44 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/hello', (req, res) => {
-  res.send('just hello.');
+  res.send('just hello.444');
 });
 
 app.get('/', (req, res) => {
-  res.send('hello World.');
+  res.send('hello World.11');
 });
 
 app.listen(process.env.PORT || 3001, () => {

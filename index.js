5bc9ba7b60221db20b91445d86585e3e1ddde78d
Mehrdad Eshraghi <67071121+MehrdadEshraghi@users.noreply.github.com>
2022-04-12 15:23:14 +0430

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/hello', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.send('W[op[op[jcom');
+  res.send('W[okkkp[op[jcom');
 });
 
 app.listen(process.env.PORT || 3001, () => {
